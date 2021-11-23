8b589724e7ef1dc339a6a1c3c95ad58acf6134d2
Khailas R <khailas303@gmail.com>
2021-11-24 01:35:32 +0530

    updated

--- car_tracker.py
+++ car_tracker.py
@@ -3,6 +3,7 @@ import cv2
 
 
 dataset = cv2.CascadeClassifier(r'V-core\cars.xml')
+dataset_2 = cv2.CascadeClassifier(r'V-core\myhaar.xml')
 video = cv2.VideoCapture(r'V-core\cars.mp4')
 
 
@@ -38,8 +39,30 @@ def multiple_car_tracker():
                     delete_car.append(car_track)
 
             for car_track in delete_car:
-                print('')
+                print(f'Removed Car ID {car_track} from List trackers')
+                car_tracker.pop(car_track, None)
+                car_side1.pop(car_track, None)
+                car_side2.pop(car_track, None)
             
+            if not (frame_counter % 20):
+                gray_scale = cv2.cvtColor(video, cv2.COLOR_BGR2GRAY)
+                cars = dataset.detectMultiScale(
+                    gray_scale,
+                    scaleFactor=1.3,
+                    minNeighbors=4,
+                    minSize=(30, 30),
+                    flags=cv2.CASCADE_SCALE_IMAGE
+                )
+                
+                for (x, y, w, h) in cars:
+                    cv2.rectangle(video,(x,y),(x+w,y+h),(255,0,0),2)
+                    roi_gray = gray_scale[y:y+h, x:x+w]
+                    roi_color = video[y:y+h, x:x+w]
+                    cars_2 = dataset_2.detectMultiScale(roi_gray) 
+                    
+                    for (ex, ey, ew, eh) in cars_2:
+                        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)    
+                
         else:
             [video] == [None]
             break
