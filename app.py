bdcf8327d5c8c4095b9e266f00ae01a100da907b
Khailas R <khailas303@gmail.com>
2022-01-30 19:57:01 +0530

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, Response, flash, send_from_directory, redirect
+from flask import Flask, render_template, request, Response, flash, redirect
 from werkzeug.utils import secure_filename
 import os
 import time
