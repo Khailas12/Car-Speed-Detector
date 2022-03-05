6618937df84f7a3ec66df10a97596acc20fb9a8c
Khailas R <khailas303@gmail.com>
2022-03-06 01:10:27 +0530

    updated

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, Response, redirect, flash, session, url_for
 from werkzeug.utils import secure_filename
-from flask_pymongo import PyMongo
 import os
 import time
 import cv2
@@ -8,28 +7,18 @@ import time
 from csv import writer
 import math
 import dlib
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
 
 
 app = Flask(__name__)
 
 app.secret_key = "secret key"
 UPLOAD_FOLDER = r"V-core\static\upload"
+
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 input = ""
 ALLOWED_VIDEO_EXTENSIONS = {"mkv", "mp4", "avi"}
-
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Batman123'
-app.config['MYSQL_DB'] = 'user_auth'
-
-mysql = MySQL(app)
 
 
 def file_allowed(filename):
@@ -40,99 +29,6 @@ def file_allowed(filename):
 
 
 @app.route("/")
-def index():
-    # Check if user is loggedin
-    if 'loggedin' in session:
-        # User is loggedin show them the home page
-        return render_template('index.html', username=session['username'])
-    # User is not loggedin redirect to login page
-    return redirect(url_for('login'))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    msg = ''
-
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(
-            'SELECT * FROM accounts WHERE username = % s', (username, ))
-        account = cursor.fetchone()
-
-        if account:
-            msg = 'Account already exists !'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address !'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form !'
-
-        else:
-            cursor.execute(
-                'INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
-            mysql.connection.commit()
-            msg = 'You have successfully registered !'
-            return redirect(url_for('index'))
-
-
-    elif request.method == 'POST':
-        msg = 'Please fill out the form !'
-    return render_template('register.html', msg=msg)
-
-
-@app.route('/profile')
-def profile():
-    # Check if user is loggedin
-    if 'loggedin' in session:
-        # We need all the account info for the user so we can display it on the profile page
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE id = %s',
-                       (session['id'],))
-        account = cursor.fetchone()
-
-        return render_template('profile.html', account=account)
-    # If the User is not loggedin redirect to login page
-    return redirect(url_for('login'))
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-
-        cursor.execute(
-            'SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
-        account = cursor.fetchone()
-
-        if account:
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            msg = 'Login Succesful'
-            return redirect(url_for('index'))
-
-        else:
-            msg = 'Incorrect Username / Password'
-    return render_template('login.html', msg=msg)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('loggedin', None)
-    session.pop('id', None)
-    session.pop('username', None)
-    return redirect(url_for('login'))
-
-
-@app.route("/upload")
 def home():
     return render_template("video_upload.html")
 
